15a9c614d2b16a7f758767d858bd6988c153c34d
Cameron <43302238+biggant1@users.noreply.github.com>
2022-03-09 02:30:52 -0800

    minor aesthetic changes

--- src/Components/Scouting/YesNo.js
+++ src/Components/Scouting/YesNo.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Yes from 'Assets/Yes.png'
-import No from 'Assets/No.png'
-import './YesNo.css'
+import React from "react";
+import Yes from "Assets/Yes.png";
+import No from "Assets/No.png";
+import "./YesNo.css";
 
-function YesNo({title, i, isYes, onInput}) {    
-    let handleClick = () => {
-        onInput(i, !isYes);
-    }
+function YesNo({ title, i, isYes, onInput }) {
+  let handleClick = () => {
+    onInput(i, !isYes);
+  };
 
-    return (
-      <div className="yesNo">
-          <img alt="" src={isYes ? Yes : No} onClick={handleClick}/>
-          <p>{title}</p>
-      </div>
-    );
+  return (
+    <div className="yesNoScout">
+      <img alt="" src={isYes ? Yes : No} onClick={handleClick} />
+      <p>{title}</p>
+    </div>
+  );
 }
 
 export default YesNo;
